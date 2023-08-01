9cdd333ccbdbf6a67d6c695361f6ddae56178777
EC2 Default User <ec2-user@ip-172-31-30-47.eu-west-1.compute.internal>
2023-08-01 17:44:58 +0000

    api added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "todoreactamplify": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
